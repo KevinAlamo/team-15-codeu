65198a2786003330d1ea406c2fedb06f1043a0fd
Kevin Alamo <kevalamop777@gmail.com>
2019-05-28 21:16:48 -0600

    Fixed a mistake I made by putting the code in the wrong files

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -6,11 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
 /**
  * Responds with a hard-coded message for testing purposes.
  */
@@ -21,14 +17,6 @@ public class StatsPageServlet extends HttpServlet{
  public void doGet(HttpServletRequest request, HttpServletResponse response)
    throws IOException {
   
-  response.getOutputStream().println("Total number of Messages: " + getTotalMessageCount());
- }
- 
- /** Returns the total number of messages for all users. */
- public int getTotalMessageCount(){
-	 DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-	 Query query = new Query("Message");
-	 PreparedQuery results = datastore.prepare(query);
-	 return results.countEntities(FetchOptions.Builder.withLimit(1000));
+  response.getOutputStream().println("hello world");
  }
 }
