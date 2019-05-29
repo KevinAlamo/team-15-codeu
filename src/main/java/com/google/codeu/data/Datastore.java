78d945aa6ebdc1f31a3f413046d7656920214739
sirisjo <siri.sjo@gmail.com>
2019-05-29 16:48:59 -0500

    added a function to get set of users who have sent a message

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -24,7 +24,9 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 /** Provides access to the data stored in Datastore. */
@@ -96,4 +98,14 @@ public class Datastore {
     }
     return messages;
   }
+
+  public Set<String> getUsers() {
+	  Set<String> users = new HashSet<>();
+	  Query query = new Query("Message");
+	  PreparedQuery results = datastore.prepare(query);
+	  for(Entity entity : results.asIterable()) {
+	    users.add((String) entity.getProperty("user"));
+	  }
+	  return users;
+	}
 }
