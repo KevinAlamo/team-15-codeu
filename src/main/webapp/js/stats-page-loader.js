d9f72281e4fe2192d0629fb3983ba34ebdbc013a
Kevin Alamo <kevalamop777@gmail.com>
2019-06-01 22:15:44 -0600

    fixed the formatting errors

--- src/main/webapp/js/stats-page-loader.js
+++ src/main/webapp/js/stats-page-loader.js
@@ -13,12 +13,12 @@
     }
 
     function buildStatElement(statString){
-     const statElement = document.createElement('p');
-     statElement.appendChild(document.createTextNode(statString));
-     return statElement;
+      const statElement = document.createElement('p');
+      statElement.appendChild(document.createTextNode(statString));
+      return statElement;
     }
 
     // Fetch data and populate the UI of the page.
     function buildUI(){
-     fetchStats();
+      fetchStats();
     }
