5116801b4af1f3d5ba707498cbebe952cefd7dc0
sirisjo <siri.sjo@gmail.com>
2019-06-13 05:37:50 +0100

    map can now display many coordinates from a .csv

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -97,10 +97,22 @@ function buildUI() {
   fetchMessages();
 }
 
-/**fetches map data */
-function createMap(){
-	const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 48.463, lng: -122.605},
-        zoom: 15
-	});
-}
+/**Fetches UFO data and creates map */
+function createUfoSightingsMap(){
+      fetch('/ufo-data').then(function(response) {
+        return response.json();
+      }).then((ufoSightings) => {
+
+        const map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 35.78613674, lng: -119.4491591},
+          zoom:7
+        });
+
+        ufoSightings.forEach((ufoSighting) => {
+          new google.maps.Marker({
+            position: {lat: ufoSighting.lat, lng: ufoSighting.lng},
+            map: map
+          });
+        });
+      });
+    }
