6e3f0d96129f28578c090febecd7cc81e34ca3ae
Connie Wu <cw342@duke.edu>
2019-06-14 14:32:37 -0700

    reading in book chart data and outputting in log

--- src/main/webapp/js/chart-loader.js
+++ src/main/webapp/js/chart-loader.js
@@ -1,23 +1,12 @@
 google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
-function drawChart(){
-  var book_data = new google.visualization.DataTable();
-  //define columns for the DataTable instance
-  book_data.addColumn('string', 'Book Title');
-  book_data.addColumn('number', 'Votes');
 
-  //add data to book_data
-  book_data.addRows([
-    ["The Best We Could Do", 6],
-    ["Sing, Unburied, Sing", 10],
-    ["The Book of Unknown Americans", 7],
-    ["The 57 Bus", 4],
-    ["The Handmaid's Tale", 8]
-  ]);
-  var chart = new google.visualization.BarChart(document.getElementById('book_chart'));
-  var chart_options = {
-    width: 800,
-    height: 400
-  };
-  chart.draw(book_data, chart_options);
+function drawChart(){
+  fetch("/new-residents-charts")
+  .then((response) => {
+    return response.json();
+})
+.then((bookJson) => {
+    console.log(bookJson);
+});
 }
