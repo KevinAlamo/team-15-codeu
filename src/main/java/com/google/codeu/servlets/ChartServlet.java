9506ecd147eba9f1b99e2f564c0e8a6f58018105
Connie Wu <cw342@duke.edu>
2019-06-16 13:12:07 -0700

    fixed formatting

--- src/main/java/com/google/codeu/servlets/ChartServlet.java
+++ src/main/java/com/google/codeu/servlets/ChartServlet.java
@@ -1,13 +1,13 @@
 package com.google.codeu.servlets;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import java.io.IOException;
+import java.util.Scanner;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import java.util.Scanner;
 
 /** Handles fetching all messages for the public feed. */
 @WebServlet("/new-residents-charts")
@@ -16,11 +16,11 @@ public class ChartServlet extends HttpServlet {
   private JsonArray bookRatingArray;
 
   // This class could be its own file if we needed it outside this servlet
-  private static class bookRating {
+  private static class BookRating {
     int freedom;
     int happy;
 
-    private bookRating(int freedom, int happy) {
+    private BookRating(int freedom, int happy) {
       this.freedom = freedom;
       this.happy = happy;
     }
@@ -30,20 +30,19 @@ public class ChartServlet extends HttpServlet {
   public void init() {
     bookRatingArray = new JsonArray();
     Gson gson = new Gson();
-    Scanner scanner =
-        new Scanner(getServletContext().getResourceAsStream("/WEB-INF/world-happiness-report-2019.csv"));
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/happiness.csv"));
     scanner.nextLine(); // skips first line (the csv header)
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
       String[] cells = line.split(",");
 
-      int freedom=0;
+      int freedom = 0;
       if (!cells[5].equals("")) {
         freedom = Integer.parseInt(cells[5]);
       }
       int happy = Integer.parseInt(cells[1]);
 
-      bookRatingArray.add(gson.toJsonTree(new bookRating(freedom, happy)));
+      bookRatingArray.add(gson.toJsonTree(new BookRating(freedom, happy)));
     }
     scanner.close();
   }
--- src/main/webapp/js/chart-loader.js
+++ src/main/webapp/js/chart-loader.js
@@ -6,28 +6,28 @@ function drawChart(){
     .then((response) => {
       return response.json();
     }).then((bookJson) => {
-    var data = new google.visualization.DataTable();
-    //define columns for the DataTable instance
-    data.addColumn('number', 'Freedom');
-    data.addColumn('number', 'Happiness');
+      var data = new google.visualization.DataTable();
+      //define columns for the DataTable instance
+      data.addColumn('number', 'Freedom');
+      data.addColumn('number', 'Happiness');
 
-    for (i = 0; i < bookJson.length; i++) {
-      row = [];
-      var freedom = bookJson[i].freedom;
-      var happy = bookJson[i].happy;
-      row.push(freedom, happy);
+      for (i = 0; i < bookJson.length; i++) {
+        row = [];
+        var freedom = bookJson[i].freedom;
+        var happy = bookJson[i].happy;
+        row.push(freedom, happy);
 
-      data.addRow(row);
-    }
+        data.addRow(row);
+      }
 
-    var options = {
-      title: 'Happpiness vs Freedom',
-      hAxis: {title: 'Freedom', minValue: 0, maxValue: 200},
-      vAxis: {title: 'Happiness', minValue: 0, maxValue: 200},
-      trendlines: { 0: {} }
-    };
+      var options = {
+        title: 'Happpiness vs Freedom',
+        hAxis: {title: 'Freedom', minValue: 0, maxValue: 200},
+        vAxis: {title: 'Happiness', minValue: 0, maxValue: 200},
+        trendlines: { 0: {} }
+      };
 
-    var chart = new google.visualization.ScatterChart(document.getElementById('chart_div2'));
-    chart.draw(data, options);
-  })
+      var chart = new google.visualization.ScatterChart(document.getElementById('chart_div2'));
+      chart.draw(data, options);
+    })
 }
