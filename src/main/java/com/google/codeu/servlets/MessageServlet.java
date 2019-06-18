3b77742506a993897837e2c4beda45da78699466
KevinAlamo <31712869+KevinAlamo@users.noreply.github.com>
2019-06-17 22:55:43 -0600

    Update MessageServlet.java

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -135,8 +135,8 @@ public class MessageServlet extends HttpServlet {
     for (BlobKey blobKey: blobKeys) {
       String fileType = new BlobInfoFactory().loadBlobInfo(blobKey).getContentType().toString();
       fileType = fileType.toLowerCase();
-      if (!(fileType.equals("image/png") || fileType.equals("image/jpg") || fileType.equals("image/gif") 
-            || fileType.equals("image/jpeg"))) {
+      if (!(fileType.equals("image/png") || fileType.equals("image/jpg") ||
+           fileType.equals("image/gif") || fileType.equals("image/jpeg"))) {
         //not yet supported so deleted
         blobstoreService.delete(blobKey);
       }
