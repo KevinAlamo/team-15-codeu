ffde1d64559ed371e75508edaf14ee2d1a4bbaf6
KevinAlamo <31712869+KevinAlamo@users.noreply.github.com>
2019-06-17 18:31:16 -0600

    Update BlobstoreUploadUrlServlet.java
    format errors

--- src/main/java/com/google/codeu/servlets/BlobstoreUploadUrlServlet.java
+++ src/main/java/com/google/codeu/servlets/BlobstoreUploadUrlServlet.java
@@ -1,5 +1,6 @@
 package com.google.codeu.servlets;
-
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import java.io.IOException;
 
 import javax.servlet.annotation.WebServlet;
@@ -7,8 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.blobstore.BlobstoreService;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
 
 @WebServlet("/blobstore-upload-url")
 public class BlobstoreUploadUrlServlet extends HttpServlet {
@@ -16,7 +16,7 @@ public class BlobstoreUploadUrlServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/my-form-handler") ;
+    String uploadUrl = blobstoreService.createUploadUrl("/my-form-handler");
 
     response.setContentType("text/html");
     response.getOutputStream().println(uploadUrl);
